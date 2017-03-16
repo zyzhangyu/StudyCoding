8632a8c85ffb9cf21dc084383e841d06ceba784e
zyzhangyu <zylike@hotmail.com>
2017-03-16 14:23:37 +0800

    1.使用XCode创建了一个基础工程

---
+++ StudyCoding/StudyCoding/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StudyCoding
+//
+//  Created by ZhangYu on 2017/3/16.
+//  Copyright © 2017年 ZhangYu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
